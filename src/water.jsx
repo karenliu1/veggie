1391ccec8ccb3c4170a13b36622c374b15967c0d
Karen Liu <karenliu1@gmail.com>
2016-03-08 21:59:13 -0800

    Add cow and shower icons to water slide.

--- src/water.jsx
+++ src/water.jsx
@@ -9,11 +9,15 @@ export default React.createClass({
                 <div className={ styles.wave2 }></div>
                 <div className={ styles.wave3 }></div>
                 <div className={ styles.waveBackground }></div>
-                <p className={ styles.textContainer }>
+                <div className={ styles.textContainer }>
+                    <div className={ styles.iconContainer }>
+                        <img src="images/cow.svg" className={ styles.icon } />
+                        <img src="images/shower.svg" className={ styles.icon } />
+                    </div>
                     It takes <strong>244</strong> gallons of water to produce a
                     pound of tofu, but <strong>2,400</strong> gallons to produce 1
                     pound of beef. So skimp on the beef, not on the showering!
-                </p>
+                </div>
             </div>
         )
     }
