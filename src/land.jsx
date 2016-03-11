fe48cb810243c8a846f2fba66f9f29c709fd2756
Karen Liu <karenliu1@gmail.com>
2016-03-10 23:46:34 -0800

    Big fish eating small fish animation.

--- src/land.jsx
+++ src/land.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import classnames from 'classnames'
 import wrapperStyles from './wrapper.less'
+import styles from './land.less'
 
 export default React.createClass({
     render() {
         return (
-            <div className={ wrapperStyles.section }>
+            <div className={ classnames('wow', 'animated', wrapperStyles.section) }>
                 It takes up to 16 pounds of grain to produce 1 pound of meat, and 5
                 pounds of wild-caught fish to produce 1 pound of farmed fish.
+
+                <div className={ styles.fishTank }>
+                    <div className={ styles.bigFish }>
+                        <img src="images/fish.svg" />
+                    </div>
+                    <div className={ styles.littleFish }>
+                        <img src="images/fish.svg" />
+                        <img src="images/fish.svg" />
+                        <img src="images/fish.svg" />
+                        <img src="images/fish.svg" />
+                        <img src="images/fish.svg" />
+                    </div>
+                </div>
 
                 The land used for animals and animal feed uses 30% of Earth's land mass.
             </div>
