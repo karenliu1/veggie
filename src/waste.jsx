6c3e2df8501522413a762eeed815e48f0467fe03
Karen Liu <karenliu1@gmail.com>
2016-03-10 16:48:35 -0800

    Add waste slide with chart animation.

--- src/waste.jsx
+++ src/waste.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import styles from './waste.less'
 import wrapperStyles from './wrapper.less'
 
 export default React.createClass({
     render() {
         return (
-            <div className={ wrapperStyles.section }>
+            <div className={ `${wrapperStyles.section} wow` }>
                 Animals raised for food produce ~130 times the amount of excrement as
                 the entire human population.
+                <div className={ styles.chartRow }>
+                    <img className={ styles.sourceIcon } src="images/pig.svg" />
+                    <div className={ styles.animalPoopBar }></div>
+                </div>
+                <div className={ styles.chartRow }>
+                    <img className={ styles.sourceIcon } src="images/people.svg" />
+                    <div className={ styles.peoplePoopBar }></div>
+                </div>
             </div>
         )
     }
