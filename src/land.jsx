237667f93b562b6a184bb5b1ee1c2c3ac3a6f39d
Karen Liu <karenliu1@gmail.com>
2016-03-12 00:14:28 -0800

    Better application of wow.js.

--- src/land.jsx
+++ src/land.jsx
@@ -6,20 +6,22 @@ import styles from './land.less'
 export default React.createClass({
     render() {
         return (
-            <div className={ classnames('wow', 'animated', wrapperStyles.section) }>
+            <div className={ wrapperStyles.section }>
                 It takes up to 16 pounds of grain to produce 1 pound of meat, and 5
                 pounds of wild-caught fish to produce 1 pound of farmed fish.
 
                 <div className={ styles.fishTank }>
-                    <div className={ styles.bigFish }>
-                        <img src="images/fish.svg" />
-                    </div>
-                    <div className={ styles.littleFish }>
-                        <img src="images/fish.svg" />
-                        <img src="images/fish.svg" />
-                        <img src="images/fish.svg" />
-                        <img src="images/fish.svg" />
-                        <img src="images/fish.svg" />
+                    <div className="wow">
+                        <div className={ styles.bigFish }>
+                            <img src="images/fish.svg" />
+                        </div>
+                        <div className={ styles.littleFish }>
+                            <img src="images/fish.svg" />
+                            <img src="images/fish.svg" />
+                            <img src="images/fish.svg" />
+                            <img src="images/fish.svg" />
+                            <img src="images/fish.svg" />
+                        </div>
                     </div>
                 </div>
 
--- src/water.jsx
+++ src/water.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import classnames from 'classnames'
 import styles from './water.less'
 import wrapperStyles from './wrapper.less'
 
 export default React.createClass({
     render() {
         return (
-            <div className={ `${wrapperStyles.section} wow ${styles.waterSection}` }>
-                <div className={ styles.wave1 }></div>
-                <div className={ styles.wave2 }></div>
-                <div className={ styles.wave3 }></div>
-                <div className={ styles.waveBackground }></div>
-                <div className={ styles.textContainer }>
+            <div className={ classnames(wrapperStyles.section, styles.waterSection) }>
+                <div className="wow">
+                    <div className={ styles.wave1 }></div>
+                    <div className={ styles.wave2 }></div>
+                    <div className={ styles.wave3 }></div>
+                    <div className={ styles.waveBackground }></div>
                     <img src="images/cow.svg" className={ styles.cow } />
+                </div>
+                <div className={ styles.textContainer }>
                     It takes <strong>244</strong> gallons of water to produce a
                     pound of tofu, but <strong>2,400</strong> gallons to produce 1
                     pound of beef. Smucky encourages you to skimp on the beef,
