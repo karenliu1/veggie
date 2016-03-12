19b29711609a1eed3b8c2fdb4b042e57ffe25f3f
Karen Liu <karenliu1@gmail.com>
2016-03-12 12:27:47 -0800

    Revise poop slide animations.

--- src/waste/index.jsx
+++ src/waste/index.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
+import classnames from 'classnames'
 import styles from './styles.less'
 import wrapperStyles from '../wrapper.less'
+import poopSvg from './poop_svg.jsx'
+import poopShockedSvg from './poop_shocked_svg.jsx'
 
 export default React.createClass({
     render() {
         return (
-            <div className={ `${wrapperStyles.section} wow` }>
-                Animals raised for food produce ~130 times the amount of excrement as
-                the entire human population.
-                <div className={ styles.chartRow }>
-                    <img className={ styles.sourceIcon } src="images/pig.svg" />
-                    <div className={ styles.animalPoopBar }></div>
-                </div>
-                <div className={ styles.chartRow }>
-                    <img className={ styles.sourceIcon } src="images/people.svg" />
-                    <div className={ styles.peoplePoopBar }></div>
+            <div className={ wrapperStyles.section }>
+                Animals raised for food produce ~130 times the amount of
+                excrement as the entire human population. Runoff pollutes
+                waterways and creates dead zones.
+
+                <div className={ classnames(styles.images, "row center-xs bottom-xs") }>
+                    <div className="col-xs-2">
+                        <img src="images/pig.svg" />
+                    </div>
+                    <div className="col-xs-6">
+                        { poopSvg }
+                    </div>
+                    <div className="col-xs-2">
+                        { poopShockedSvg }
+                    </div>
+                    <div className="col-xs-2">
+                        <img src="images/sweat.svg" />
+                    </div>
                 </div>
             </div>
         )
---
+++ src/waste/poop_svg.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './styles.less'
+
+export default (
+    <svg viewBox="0 0 60 60" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="poop">
+                <path d="M30.206,34.887 C13.146,34.887 0,27.789 0,43.728 C0,56.456 16.892,60 29.789,60 C44.773,60 60,56.456 60,43.728 C60,27.789 47.839,34.887 30.206,34.887 L30.206,34.887 Z" id="Shape" fill="#89664C"></path>
+                <path d="M29.729,18.879 C20.176,18.879 5.227,19.995 5.227,31.5 C5.227,48.326 54.772,48.326 54.772,31.5 C54.772,19.995 39.884,18.879 29.732,18.879" id="Shape" fill="#9B7861"></path>
+                <path d="M47.01,14.7 C42.352,4.36 20.643,15.006 24.372,2.474 C24.934,0.586 24.911,-0.239 23.476,0.06 C15.664,1.681 10.039,9.726 12.019,16.379 C18.395,37.812 53.299,28.652 47.01,14.7" id="Shape" fill="#A88673"></path>
+                <ellipse cx="19.604" cy="32.604" rx="7.205" ry="7.205" id="Shape" fill="#FFFFFF"></ellipse>
+                <ellipse className={ styles.poopEyePath } id="Oval" fill="#000000" cx="19.604" cy="32.604" rx="3.604" ry="3.604"></ellipse>
+                <ellipse cx="40.37" cy="32.32" rx="7.205" ry="7.205" id="Shape" fill="#FFFFFF"></ellipse>
+                <ellipse className={ styles.poopEyePath } id="Oval" fill="#000000" cx="40.37" cy="32.32" rx="3.604" ry="3.603"></ellipse>
+            </g>
+        </g>
+    </svg>
+)
