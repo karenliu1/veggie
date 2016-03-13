17edc20f30e47216326e7c3f9678e1c2b299b825
Karen Liu <karenliu1@gmail.com>
2016-03-12 23:03:33 -0800

    Remove relieved face transition.

--- src/weekday/index.jsx
+++ src/weekday/index.jsx
@@ -4,27 +4,13 @@ import styles from './styles.less'
 import crySvg from './cry_svg.jsx'
 
 export default React.createClass({
-    getInitialState() {
-        return {
-            isRelieved: false
-        }
-    },
-
-    changeFace(e) {
-        this.setState({ isRelieved: true })
-        e.preventDefault()
-    },
-
     render() {
         return (
             <div>
                 <div className="row">
                     <div className="col-xs-6">
                         <div className={ styles.faceIconContainer }>
-                            { this.state.isRelieved ? (
-                                <img src="images/relieved.svg"
-                                     className={ classnames("icon", styles.relievedIcon) } />
-                            ) : crySvg }
+                            { crySvg }
                         </div>
                     </div>
                     <div className="col-xs-6">
@@ -35,10 +21,7 @@ export default React.createClass({
                     "So does this mean I have to give up burgers forever??"
                 </p>
                 <p>
-                    <a href="#" onClick={ this.changeFace }>Not necessarily</a>.
-                </p>
-                <p>
-                    Graham Hill has a <a
+                    Not necessarily. Graham Hill has a <a
                         href="https://www.ted.com/talks/graham_hill_weekday_vegetarian"
                         target="_blank">wonderful TED talk</a> about
                     making an incremental change. Cutting back partway
