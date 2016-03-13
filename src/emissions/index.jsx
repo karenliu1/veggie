66653ccbb994d4d09dee670c8cd4e17de7bf3dd6
Karen Liu <karenliu1@gmail.com>
2016-03-13 03:04:53 -0700

    More mobile friendly emissions slide.

--- src/emissions/index.jsx
+++ src/emissions/index.jsx
@@ -6,51 +6,24 @@ import boatSvg from './boat_svg.jsx'
 import planeSvg from './plane_svg.jsx'
 
 export default React.createClass({
-    getInitialState() {
-        return {
-            showAnimals: false
-        }
-    },
-
-    showAnimals(e) {
-        this.setState({ showAnimals: true })
-        e.preventDefault()
-    },
-
     render() {
-        const animalsEl = (
-            <div className="row center-xs">
-                <div className="col-xs-4">
-                    <img className={ styles.chickenIcon } src="images/chicken.svg" />
-                </div>
-            </div>
-        )
-
-        const transportationClasses = classnames(
-            this.state.showAnimals && styles.shrinkOut,
-            styles.transportationContainer
-        )
-
         return (
             <div>
-                The greenhouse gas emissions produced by animal farming <a
-                    href="#" onClick={ this.showAnimals }>are more than the
-                    emissions</a> from all cars, trucks, planes and
+                The greenhouse gas emissions produced by animal farming are
+                more than the emissions from all cars, trucks, planes and
                 ships <strong>combined</strong>.
 
-                <div className={ transportationClasses }>
-                    <div className={ styles.planeIcon }>
+                <div className="row center-xs bottom-xs">
+                    <div className={ classnames("col-xs-12 col-sm-4", styles.planeIcon) }>
                         { planeSvg }
                     </div>
-                    <div className={ styles.carIcon }>
+                    <div className={ classnames("col-xs-12 col-sm-4", styles.carIcon) }>
                         { carSvg }
                     </div>
-                    <div className={ styles.boatIcon }>
+                    <div className={ classnames("col-xs-12 col-sm-4", styles.boatIcon) }>
                         { boatSvg }
                     </div>
                 </div>
-
-                { this.state.showAnimals && animalsEl }
             </div>
         )
     }
