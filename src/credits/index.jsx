68e0bc627d9e64d72008fa0d44c6b88ea4555464
Karen Liu <karenliu1@gmail.com>
2016-03-12 16:33:37 -0800

    Clean up credits slide.

--- src/credits/index.jsx
+++ src/credits/index.jsx
@@ -2,17 +2,35 @@ import React from 'react'
 import classnames from 'classnames'
 import wrapperStyles from '../wrapper.less'
 
+const FACT_URL = "http://www.onegreenplanet.org/animalsandnature/facts-on-animal-farming-and-the-environment/"
+const EMOJI_URL = "http://emojione.com/"
+
 export default React.createClass({
     render() {
         return (
             <div className={ wrapperStyles.section }>
-                <p>
-                    All the images are from the gallery at EmojiOne:
-                    <a href="http://emojione.com/" target="_blank">EmojiOne</a>
-                </p>
-                <p>
-                    Factoids nabbed from <a href="http://www.onegreenplanet.org/animalsandnature/facts-on-animal-farming-and-the-environment/" target="_blank">One Green Planet</a>
-                </p>
+                <div className="row middle-xs">
+                    <div className="col-xs-12 col-sm-9">
+                        Images sourced and edited from the beautiful gallery at <a
+                            href={ EMOJI_URL }>EmojiOne</a>
+                    </div>
+                    <div className="col-xs-12 col-sm-3">
+                        <a href={ EMOJI_URL }>
+                            <img className="icon" src="images/croc.svg" />
+                        </a>
+                    </div>
+                </div>
+                <div className="row middle-xs">
+                    <div className="col-xs-12 col-sm-3">
+                        <a href={ FACT_URL }>
+                            <img className="icon" src="images/plant.svg" />
+                        </a>
+                    </div>
+                    <div className="col-xs-12 col-sm-9">
+                        Factoids nabbed from <a
+                            href={ FACT_URL }>One Green Planet</a>
+                    </div>
+                </div>
             </div>
         )
     }
